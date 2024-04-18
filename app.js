04f162cf4b63f5c84018bab466068d9ace86f199
MariaDoina <152196778+MariaDoina@users.noreply.github.com>
2024-04-18 15:11:15 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const clothesRoutes = require("./routes/clothesRoutes");
+
+//conencting to mongoDB
+const dbURI =
+  "mongodb+srv://Maria:proiectDB@store.tnbq3ln.mongodb.net/ClothingStore?retryWrites=true&w=majority&appName=Store";
+
+mongoose
+  .connect(dbURI)
+  .then((result) => app.listen(5050))
+  .catch((err) => console.log(err));
+//express app
+const app = express();
+
+//register view engine
+app.set("view engine", "ejs");
+
+//middleware and static files(for style.css)
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+//routes
+app.get("/", (req, res) => {
+  res.render("index", { title: "Home" });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About us" });
+});
+
+app.get("/women", (req, res) => {
+  res.render("women", { title: "Women" });
+});
+
+//clothes routes
+app.use("/clothes", clothesRoutes);
+
+//404 page
+app.use((req, res) => {
+  res.status(404).render("404", { title: "404" });
+});
